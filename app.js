18fe8ae1d4a3c46c21ca8ae0346e420e766788c4
280b9f9b <280b9f9b@gmail.com>
2022-06-01 18:27:11 +0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+
+        fetch('app.json')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            appendData(data);
+        })
+        .catch(function (err) {
+            console.log('error: ' + err);
+        });
+    
+    
+     
+
+function gameTemplate(gamex) {
+  return `
+    <div class="GameBox"><a href="${gamex.game}"><img class="GameCover" src="${gamex.photo}"></a>
+    <h2 class="GameText">${gamex.name}</h2></div>
+  `;
+}
+function appendData(data) {
+  var mainContainer = document.getElementById("app").innerHTML = `
+<h1 class="GameHeader">EmulatorJS (${data.length})</h1>
+  ${data.map(gameTemplate).join("")}
+  <p class="GameFooter">These ${data.length} Games were added recently. Check back soon for updates.</p>
+`;
+}
